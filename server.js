fe8cc22bf391de4bb6eaeb01cef8bd461c3d96f6
Sumit Gupta <sumitg979230@gmail.com>
2026-06-29 00:54:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-
 const app = express();
+app.use(express.static(__dirname));
 const server = http.createServer(app);
 const io = new Server(server, { 
     cors: { origin: "*" } 
